aa23eca57af0f50884ef6bab6077200edbea346e
Pedro Varela <pedrogdvarela@gmail.com>
2018-12-18 19:57:38 +0000

    last changes

---
+++ lovers.sql
@@ -0,0 +1,8 @@
+{"id":3,"version":0,"name":"Vasco Melro","age":25,"city":"Matosinhos","gender":"Male","eyeColor":"Brown","height":165,"image":"https://randomuser.me/api/portraits/men/43.jpg"}
+{"id":4,"version":0,"name":"Mónica Modista","age":28,"city":"Porto","gender":"Female","eyeColor":"Green","height":172,"image":"https://randomuser.me/api/portraits/women/52.jpg"}
+{"id":5,"version":0,"name":"António Castro","age":30,"city":"Aveiro","gender":"Male","eyeColor":"Brown","height":178,"image":"https://randomuser.me/api/portraits/men/20.jpg"}
+{"id":6,"version":0,"name":"Júlia Moreira","age":34,"city":"Braga","gender":"Female","eyeColor":"Brown","height":169,"image":"https://randomuser.me/api/portraits/women/29.jpg"}
+{"id":7,"version":0,"name":"João Carlos","age":31,"city":"Atouguia da Baleia","gender":"Male","eyeColor":"Blue","height":180,"image":"https://randomuser.me/api/portraits/men/84.jpg"}
+{"id":8,"version":0,"name":"Catarina Alves","age":24,"city":"Gondomar","gender":"Female","eyeColor":"Brown","height":175,"image":"https://randomuser.me/api/portraits/women/59.jpg"}
+{"id":9,"version":0,"name":"Rui António","age":30,"city":"Porto","gender":"Male","eyeColor":"Blue","height":170,"image":"https://randomuser.me/api/portraits/men/9.jpg"}
+{"id":10,"version":0,"name":"Núria Almeida","age":33,"city":"Viseu","gender":"Female","eyeColor":"Green","height":177,"image":"https://randomuser.me/api/portraits/women/79.jpg"}
